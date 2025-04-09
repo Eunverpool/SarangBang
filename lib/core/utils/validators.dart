483b5fca59d05aa2aa6b585e9ed49de5eefb6a54
Eunverpool <dmsdn3403@gmail.com>
2025-04-09 18:07:53 +0900

    임시 커밋

--- lib/core/utils/validators.dart
+++ lib/core/utils/validators.dart
@@ -6,15 +6,15 @@ class Validators {
 
   /// Password Validator
   static final password = MultiValidator([
-    RequiredValidator(errorText: 'Password is required'),
-    MinLengthValidator(8, errorText: 'Password must be at least 8 digits long'),
+    RequiredValidator(errorText: '패스워드를 입력하세요.'),
+    MinLengthValidator(8, errorText: '패스워드는 최소 8자리 이상이여야 합니다.'),
     PatternValidator(r'(?=.*?[#?!@$%^&*-])',
         errorText: 'Passwords must have at least one special character')
   ]);
 
   /// Required Validator with Optional Field Name
   static RequiredValidator requiredWithFieldName(String? fieldName) =>
-      RequiredValidator(errorText: '${fieldName ?? 'Field'} is required');
+      RequiredValidator(errorText: '${fieldName}를 입력하세요.');
 
   /// Plain Required Validator
   static final required = RequiredValidator(errorText: 'Field is required');
