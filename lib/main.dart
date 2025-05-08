0166581c3fc33f98115a6afeabcab5b64a5af161
Eunverpool <dmsdn3403@gmail.com>
2025-05-08 10:01:51 +0900

    0508 1001 eunwoo uuid 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // shared_preferences 패키지 추가
+import 'package:uuid/uuid.dart';
 
 import 'core/routes/app_routes.dart';
 import 'core/routes/on_generate_route.dart';
@@ -7,8 +9,42 @@ import 'core/themes/app_themes.dart';
 // 국제화 기능
 import 'package:intl/date_symbol_data_local.dart';
 
+// 기기 ID 관리 함수
+class DeviceIdManager {
+  static const String _deviceIdKey = 'device_id';
+
+  static Future<String?> getDeviceId() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_deviceIdKey);
+  }
+
+  static Future<String> generateAndSaveDeviceId() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    const uuid = Uuid();
+    final String deviceId = uuid.v4();
+    await prefs.setString(_deviceIdKey, deviceId);
+    return deviceId;
+  }
+
+  static Future<String> getOrCreateDeviceId() async {
+    final String? storedDeviceId = await getDeviceId();
+    if (storedDeviceId != null) {
+      return storedDeviceId;
+    } else {
+      return await generateAndSaveDeviceId();
+    }
+  }
+
+  static Future<void> printDeviceId() async {
+    final String deviceId = await getOrCreateDeviceId();
+    print('Device ID: $deviceId');
+  }
+}
+
 void main() {
   initializeDateFormatting().then((_) => runApp(const MyApp()));
+  // 앱 시작 시 기기 ID를 콘솔에 출력 (선택 사항)
+  DeviceIdManager.printDeviceId();
 }
 
 class MyApp extends StatelessWidget {
