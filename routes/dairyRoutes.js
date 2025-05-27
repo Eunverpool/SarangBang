76d525bb4134fa34769a39a8ad2ceea3e9b69a89
Eunverpool <dmsdn3403@gmail.com>
2025-05-27 11:17:01 +0900

    0527 1116 error 처리

--- routes/dairyRoutes.js
+++ routes/dairyRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const dairyController = require("../controllers/dairyController");
 
-router.post("/", dairyController.generateDairy);
+//router.post("/", dairyController.generateDairy);
 router.get("/", dairyController.getDairy);
 router.get("/random-diary", dairyController.getRandomDairy);
 module.exports = router;
