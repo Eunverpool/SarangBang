fc8e24e2ad67d0ac44a98bc00e620cd595180e87
Eunverpool <dmsdn3403@gmail.com>
2025-05-28 16:44:19 +0900

    0528 1644 report 디자인 수정

--- lib/views/report/report_page.dart
+++ lib/views/report/report_page.dart
@@ -4,6 +4,7 @@ import 'package:grocery/views/report/components/report_button.dart';
 import 'package:grocery/views/report/components/report_summary.dart';
 import 'package:grocery/views/report/components/report_emotion.dart';
 import 'package:grocery/views/report/components/report_cognitivetest.dart';
+import 'package:grocery/views/report/components/report_mentalstatus.dart';
 import '../../core/constants/constants.dart';
 
 import '../../data/report_data.dart';
@@ -19,27 +20,26 @@ class ReportPage extends StatelessWidget {
     if (report == null) {
       return const Center(child: Text('해당 날짜의 보고서가 없습니다.'));
     }
+
     return Scaffold(
       appBar: ReportHeader(date: selectedDate),
-      body: Padding(
+      body: SingleChildScrollView(
+        // ✅ 스크롤 가능하게 변경
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              flex: 2,
-              child: Column(
-                children: [
-                  ReportSummary(summary: report.summary),
-                  const SizedBox(height: 50),
-                  ReportEmotion(emotionRatio: report.emotionRatio),
-                ],
-              ),
+            ReportSummary(summary: report.summary),
+            const SizedBox(height: 50),
+            ReportEmotion(emotionRatio: report.emotionRatio),
+            const SizedBox(height: 20),
+            ReportMentalStatus(
+              cognitiveResult: '정상',
+              depressionScore: 0.65,
             ),
-            const Expanded(
-              flex: 1,
-              child: ReportCognitive(),
-            ),
+            const SizedBox(height: 20),
+            const ReportCognitive(),
+            const SizedBox(height: 20),
             const ReprotButton(),
           ],
         ),
